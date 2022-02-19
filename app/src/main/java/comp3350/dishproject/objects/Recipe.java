11a9bae02986569ccbdf9defa4e224b3de998bcc
jannatul5088 <60824982+jannatul5088@users.noreply.github.com>
2022-02-19 15:58:14 -0600

    Add some recipe test

--- app/src/main/java/comp3350/dishproject/objects/Recipe.java
+++ app/src/main/java/comp3350/dishproject/objects/Recipe.java
@@ -5,51 +5,86 @@ import java.util.Objects;
 
 public class Recipe {
     private String name,id,direction;
-    private double calorie;
+    private double calorie,weight;
     private ArrayList<Ingredient>ingredientArrayList;
     private int rating;
-    public Recipe(String name,String id, double calorie, ArrayList<Ingredient> ingredientArrayList){
+    /*
+    recipe class will have a name, id, calorie, weight, arraylist of Ingredients
+    it also has rating which is set to 0 and direction which is empty
+     */
+    public Recipe(String name,String id, double calorie,double weight, ArrayList<Ingredient> ingredientArrayList){
         this.name=name;
         this.id=id;
         this.calorie=calorie;
+        this.weight=weight;
         this.ingredientArrayList=ingredientArrayList;
         this.rating=0;
         this.direction="";
     }
+    /*
+    method to set weight
+     */
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    /*
+    method to get weight
+     */
+    public double getWeight() {
+        return weight;
+    }
+    /*
+    method to set direction
+     */
     public void setDirectionMethod(String s){
         this.direction=s;
     }
-
+    /*
+    method to get direction
+     */
     public String getDirection() {
         return direction;
     }
-
+    /*
+    method to set id
+     */
     public void setId(String id) {
         this.id = id;
     }
-
+    /*
+    method to get rating
+     */
     public int getRating() {
         return rating;
     }
-
+    /*
+    method to set rating
+     */
     public void setRating(int rating) {
         this.rating = rating;
     }
-
+    /*
+    method to set calorie
+     */
     public void setCalorie(double calorie) {
         this.calorie = calorie;
     }
-
+    public boolean hasIngredient(Ingredient ingredient){
+        for(int i=0;i<this.getIngredientArrayList().size();i++){
+            System.out.println(this.getIngredientArrayList().size());//this.getIngredientArrayList().get(i).getName()
+            System.out.println(this.getIngredientArrayList().get(i).getName());
+            System.out.println(this.getIngredientArrayList().get(i)!=null);
+            if(ingredient.getName().equals(this.getIngredientArrayList().get(i).getName()) && ingredient.getId().equals(this.getIngredientArrayList().get(i).getId())){
+                return true;
+            }
+        }
+        return false;
+    }
     public void setIngredientArrayList(ArrayList<Ingredient> ingredientArrayList) {
         this.ingredientArrayList = ingredientArrayList;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Recipe recipe = (Recipe) o;
-        return Double.compare(recipe.calorie, calorie) == 0 && Objects.equals(name, recipe.name) && Objects.equals(id, recipe.id) && Objects.equals(ingredientArrayList, recipe.ingredientArrayList);
+    public boolean isEquals(Recipe o) {
+        return this.name.equals(o.name) && this.id.equals(o.id);
     }
     public void removeIngredientFromRecipe(Ingredient ingredient){
         for(int i=0;i<this.ingredientArrayList.size();i++){
@@ -62,18 +97,13 @@ public class Recipe {
             }
         }
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, id, calorie, ingredientArrayList);
-    }
-
+    /*
+    method to set name
+     */
     public void setName(String name) {
         this.name = name;
     }
-    public void setCalorie(float calorie){
-        this.calorie=calorie;
-    }
+
     public void increaseCalorie(double x){
         this.calorie+=calorie;
     }
@@ -85,15 +115,21 @@ public class Recipe {
     public ArrayList<Ingredient> getIngredientArrayList() {
         return ingredientArrayList;
     }
-
+    /*
+    method to get calorie
+     */
     public double getCalorie() {
         return calorie;
     }
-
+    /*
+    method to get name
+     */
     public String getName() {
         return name;
     }
-
+    /*
+    method to get id
+     */
     public String getId() {
         return id;
     }
@@ -104,6 +140,7 @@ public class Recipe {
                 "name='" + name + '\'' +
                 ", id='" + id + '\'' +
                 ", calorie=" + calorie +
+                ", weight=" + calorie +
                 ", ingredientArrayList=" + ingredientArrayList +
                 '}';
     }
--- app/src/test/java/comp3350/dishproject/tests/objects/RecipeTest.java
+++ app/src/test/java/comp3350/dishproject/tests/objects/RecipeTest.java
@@ -1,4 +1,123 @@
 package comp3350.dishproject.tests.objects;
 import junit.framework.TestCase;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import comp3350.dishproject.objects.Ingredient;
+import comp3350.dishproject.objects.Recipe;
+
+
 public class RecipeTest {
+    private static ArrayList<Ingredient>list=new ArrayList<Ingredient>();
+    private static Ingredient i1,i2,i3;
+    private static Recipe recipe;
+    @BeforeClass
+    public static void beforeClass(){
+        Ingredient i1=new Ingredient("Orange", "1", 1, 250.0,30.0);
+        Ingredient i2=new Ingredient("Banana", "2", 2, 200.0,60.0);
+        Ingredient i3=new Ingredient("Kiwi", "3", 3, 500.0,25.0);
+        recipe=new Recipe("Fruit salad","1", 600.0,450, new ArrayList<Ingredient>());
+        recipe.addIngredient(i2);
+        recipe.addIngredient(i2);
+        recipe.addIngredient(i3);
+        //System.out.println(recipe.getIngredientArrayList());
+//        list=new ArrayList<Ingredient>();
+//        list.add(i1);
+//        list.add(i2);
+//        list.add(i3);
+    }
+    @Test
+    public void testHasIngredient(){
+        //Recipe r=new Recipe("Fruit salad","1", 600.0,450, list);
+        //System.out.println(recipe.hasIngredient(i3));
+        //r.hasIngredient(i1);
+        //assert(r.hasIngredient(i2));
+    }
+    @Test
+    public void testConstructor(){
+        Recipe r=new Recipe("Pizza","1", 600.0,450, new ArrayList<Ingredient>());
+        assertNotNull(r!=null);
+        assert(r.getRating()==0);
+        assert(r.getDirection().equals(""));
+    }
+    @Test
+    public void testGetCalorie(){
+        Recipe r=new Recipe("Pizza","1", 600.0,450, new ArrayList<Ingredient>());
+        assertEquals(600.0, r.getCalorie(), 0.0);
+        assert(r.getCalorie()!=18.0);
+    }
+    @Test
+    public void testSetCalorie(){
+        Recipe r=new Recipe("Pizza","1", 600.0,450, new ArrayList<Ingredient>());
+        assertEquals(600.0, r.getCalorie(), 0.0);
+        r.setCalorie(150.0);
+        assertEquals(150.0, r.getCalorie(), 0.0);
+
+    }
+    @Test
+    public void testGetWeight(){
+        Recipe r=new Recipe("Pizza","1", 600.0,450, new ArrayList<Ingredient>());
+        assertEquals(450.0, r.getWeight(), 0.0);
+        assert(r.getWeight()!=18.0);
+    }
+    @Test
+    public void testSetWeight(){
+        Recipe r=new Recipe("Pizza","1", 600.0,450, new ArrayList<Ingredient>());
+        assertEquals(450.0, r.getWeight(), 0.0);
+        r.setWeight(150.0);
+        assertEquals(150.0, r.getWeight(), 0.0);
+    }
+    @Test
+    public void testGetDirection(){
+        Recipe r=new Recipe("Pizza","1", 600.0,450, new ArrayList<Ingredient>());
+        assert(r.getDirection().equals(""));
+    }
+    @Test
+    public void testSetDirection(){
+        Recipe r=new Recipe("Pizza","1", 600.0,450, new ArrayList<Ingredient>());
+        r.setDirectionMethod("do this then that etc");
+        assert(r.getDirection().equals("do this then that etc"));
+
+    }
+    @Test
+    public void testGetId(){
+        Recipe r=new Recipe("Pizza","1", 600.0,450, new ArrayList<Ingredient>());
+        assert(r.getId().equals("1"));
+    }
+    @Test
+    public void testSetId(){
+        Recipe r=new Recipe("Pizza","1", 600.0,450, new ArrayList<Ingredient>());
+        r.setId("22");
+        assert(!r.getId().equals("1"));
+        assert(r.getId().equals("22"));
+    }
+    @Test
+    public void testGetRating(){
+        Recipe r=new Recipe("Pizza","1", 600.0,450, new ArrayList<Ingredient>());
+        assert(r.getRating()==0);
+    }
+    @Test
+    public void testSetRating(){
+        Recipe r=new Recipe("Pizza","1", 600.0,450, new ArrayList<Ingredient>());
+        r.setRating(4);
+        assert(r.getRating()!=0);
+        assert(r.getRating()==4);
+    }
+    @Test
+    public void testGetName(){
+        Recipe r=new Recipe("Pizza","1", 600.0,450, new ArrayList<Ingredient>());
+        assert(r.getName().equals("Pizza"));
+    }
+    @Test
+    public void testSetName(){
+        Recipe r=new Recipe("Pizza","1", 600.0,450, new ArrayList<Ingredient>());
+        r.setName("Burger");
+        assert(!r.getName().equals("Pizza"));
+        assert(r.getName().equals("Burger"));
+    }
 }
