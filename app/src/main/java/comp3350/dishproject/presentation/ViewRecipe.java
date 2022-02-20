41c3e140e70a4d1cca29f0925df73f3c86aa7bf2
Abdullah Al Noman <48934180+nomanaa@users.noreply.github.com>
2022-02-19 19:23:01 -0600

    ViewRecipe class added

---
+++ app/src/main/java/comp3350/dishproject/presentation/ViewRecipe.java
@@ -0,0 +1,16 @@
+package comp3350.dishproject.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import comp3350.dishproject.R;
+
+public class ViewRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_recipe);
+    }
+}
