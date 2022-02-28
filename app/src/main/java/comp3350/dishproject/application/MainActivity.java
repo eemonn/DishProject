1ce41225d1a1b13e2687a4134e69c25dabbcb823
Tim Sarvis <sarvist@myumanitoba.ca>
2022-02-28 21:09:22 +0000

    Added import statement back

--- app/src/main/java/comp3350/dishproject/application/MainActivity.java
+++ app/src/main/java/comp3350/dishproject/application/MainActivity.java
@@ -13,7 +13,7 @@ import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
