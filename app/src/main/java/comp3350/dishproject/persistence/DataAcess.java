438645f0cff284852aebe445b36a2bc6082527c4
S M Rukunujjaman <eemon.shine@gmail.com>
2022-03-03 14:04:33 +0600

    updated DataAcess

--- app/src/main/java/comp3350/dishproject/persistence/DataAcess.java
+++ app/src/main/java/comp3350/dishproject/persistence/DataAcess.java
@@ -1,13 +1,21 @@
 package comp3350.dishproject.persistence;
+import comp3350.dishproject.R;
 import comp3350.dishproject.presentation.item;
 import java.util.ArrayList;
 
 public class DataAcess implements fakeDB{
-    private ArrayList<item> reciPe;
-
+    public ArrayList<item> reciPe;
+    public int size;
     //constructor
     public DataAcess(){
+
         reciPe=new ArrayList<item>();
+        reciPe.add(new item(R.drawable.burger, "Bobby's Burger"));
+        reciPe.add(new item(R.drawable.pizza, "Paul's Pizza"));
+        reciPe.add(new item(R.drawable.taco, "Timmy's Taco"));
+        reciPe.add(new item(R.drawable.pancake, "Patricia's Pancake"));
+        reciPe.add(new item(R.drawable.fish, "Freddy's Fish"));
+        size=5;
     }
 
     //adding items
@@ -15,6 +23,7 @@ public class DataAcess implements fakeDB{
     public void addRecipe(item name) {
          //when we want to add new recipe to the existing ones.
             this.reciPe.add(name);
+            size++;
     }
 
     @Override
@@ -30,7 +39,7 @@ public class DataAcess implements fakeDB{
 
     @Override
     public int  removeRecipe(String name) {
-        int reTruncode=-3; // if no recipe found to delete return -3
+        int reTurncode=-3; // if no recipe found to delete return -3
         boolean track=false;
         item nameOfRecipe;
         int i=0;
@@ -39,20 +48,25 @@ public class DataAcess implements fakeDB{
             if(nameOfRecipe.getRecipeName().equals(name)){
                 track=true;
                 reciPe.remove(i);
-                reTruncode=1; // when successfully remove something it will 1
+                size--;
+                reTurncode=1; // when successfully remove something it will 1
             }
-            else{
-                if(track==false){//if not found the movie name with the index i, just increment i
+
+            if(track==false){//if not found the movie name with the index i, just increment i
                     i++;
-                }
+
             }
         }
-        return reTruncode;
+        return reTurncode;
     }
 
     //returning all items at once
     @Override
     public ArrayList<item> getAllRecipe() {
         return this.reciPe;
+    }
+
+    public int getSizeofRecipe(){
+        return this.size;
     }
 }
